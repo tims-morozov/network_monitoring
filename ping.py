568ff0963555f3e3fdb71e4e0069f160c404312c
Timur Morozov <81633311+tims-morozov@users.noreply.github.com>
2024-03-14 18:56:17 +0300

    commit_2.0

--- ping.py
+++ ping.py
@@ -1,30 +1,36 @@
 import os
+import argparse
 import time
 from termcolor import colored
 
 
-hostname = 'gmail.com'
-n = 5
+parser = argparse.ArgumentParser(description='Parse of hostname and max_attempts')
+parser.add_argument("h")
+parser.add_argument("-a")
+args = parser.parse_args()
+
+hostname = args.h 
+max_attempts = args.a
+
 counter = 0
 
+def propeller():
+    blades = ['|', '/', '—', '\\']
+    for _ in range(2):
+        for i in blades:
+            print(i, end='\r', flush=True)
+            time.sleep(0.5)
+        
 while True: 
     try:
-        print('|', end='\r', flush=True)
-        time.sleep(0.5)
-        print('/', end='\r', flush=True)
-        time.sleep(0.5)
-        print('—', end='\r', flush=True)
-        time.sleep(0.5)
-        print('\\', end='\r', flush=True)
-        time.sleep(0.5)
-        print('|', end='\r', flush=True)
-        time.sleep(0.5)
+        propeller()
+        
         response = os.system("ping -c 1 " + hostname + '>/dev/null 2>&1')
         if response !=0:
             counter += 1
         else:
             counter = 0
-        if counter == n:
+        if counter == max_attempts:
             print(colored(' Low', 'red'), end='\r', flush=True)
         else:
             print(colored(' Ok ', 'green'), end='\r', flush=True)
